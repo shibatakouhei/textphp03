4beb2fe8df4f0524ca20c91b92af24acb48087bf
shibatakouhei <mk07080802@gmail.com>
2020-11-20 06:42:12 +0000

    second text

---
+++ php03.php
@@ -0,0 +1,8 @@
+<?php
+$height = 160;
+if ($height < 150) {
+echo "150cm 未満の方はご乗車できません。";
+}
+else {
+echo "ご乗車できます。";
+}
--- textphp02.php
+++ textphp02.php
@@ -1,5 +1,5 @@
 <?php
-
+// 課題１
 $a = 3;
 echo $a;
 echo "\n";
@@ -8,11 +8,11 @@ echo $b;
 echo "\n";
 echo $a+$b;
 echo "\n";
-
+// 課題２
 $array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月",];
 echo $array_month [7];
 echo "\n";
-
+// 課題３
 $hello = "Hello"; 
 echo $hello;
 $name = "[柴田康平]";
@@ -20,12 +20,12 @@ echo $name;
 $word = "'s World!";
 echo $word;
 echo "\n";
-
+// 課題４
 $tech_boost = "tech";
 $tech_boost .= "boost";
 echo $tech_boost;
 echo "\n";
-
+// 課題５
 $２０１８_calender = [
     
 "January" => "1月",
